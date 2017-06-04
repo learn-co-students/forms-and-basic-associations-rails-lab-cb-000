03729d31caa66c4d8c19d348377d286152203cea
jwald1 <email@example.com>
2017-06-04 16:42:08 -0400

    model specs passes

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,5 +2,34 @@ class Song < ActiveRecord::Base
   belongs_to :artist
   belongs_to :genre
   has_many :notes
-end
 
+  def genre_name=(name)
+    self.genre ||= Genre.create
+    genre.name = name
+  end
+
+  def genre_name
+    genre.name
+  end
+
+  def artist_name=(name)
+    self.artist ||= Artist.create
+    artist.name = name
+  end
+
+  def artist_name
+    artist.name
+  end
+
+  def note_contents
+    contents = notes.pluck(:content)
+
+    contents.empty? ? notes.map(&:content) : contents
+  end
+
+  def note_contents=(contents)
+    contents.each do |content|
+      notes.build(content: content) unless content.empty?
+    end
+  end
+end
