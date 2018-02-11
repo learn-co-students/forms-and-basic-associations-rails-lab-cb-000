97868a96cde0e44d2cc5e588bb054c2429054ab0
Heather Christman <efl7a@yahoo.de>
2018-02-11 15:18:12 -0500

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,5 +2,35 @@ class Song < ActiveRecord::Base
   belongs_to :artist
   belongs_to :genre
   has_many :notes
-end
 
+  validates :title, :artist, presence: true
+
+  def genre_name
+    self.genre.name
+  end
+
+  def genre_name=(name)
+    self.genre = Genre.find_or_create_by(name: name)
+  end
+
+  def artist_name
+    self.artist.name unless self.artist == nil
+  end
+
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by(name: name)
+  end
+
+  def note_contents
+    self.notes.map do |note|
+      note.content
+    end
+  end
+
+  def note_contents=(notes)
+    notes.reject(&:blank?).each do |note|
+      n = Note.find_or_create_by(content: note)
+      self.notes << n
+    end
+  end
+end
