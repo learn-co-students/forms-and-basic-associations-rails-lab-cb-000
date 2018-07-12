503382fd7668f8cde86a9797700566398cb7f5c2
Alex Segers <a.segers.dev@gmail.com>
2018-07-12 22:32:37 +0000

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,5 +2,21 @@ class Song < ActiveRecord::Base
   belongs_to :artist
   belongs_to :genre
   has_many :notes
-end
 
+  def genre_name=(name)
+    genre = Genre.find_or_create_by(name: name)
+  end
+
+  def genre_name
+    genre ? genre.name : nil
+  end
+
+  def artist_name=(name)
+    artist = Artist.find_or_create_by(name: name)
+  end
+
+  def artist_name
+    artist ? artist.name : nil
+  end
+
+end
