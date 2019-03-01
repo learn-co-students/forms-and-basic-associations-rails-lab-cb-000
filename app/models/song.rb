2d12fde127a3fd8161782d9eee918d9753c26f31
Sergio Marrocoli <s.marrocoli@gmail.com>
2019-03-01 12:05:11 +0000

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -5,7 +5,7 @@ class Song < ActiveRecord::Base
   has_many :notes
 
   def genre_name=(name)
-    self.genre = Genre.find_or_create_by(name: genre_name)
+    self.genre = Genre.find_or_create_by(name: name)
   end
 
   def genre_name
